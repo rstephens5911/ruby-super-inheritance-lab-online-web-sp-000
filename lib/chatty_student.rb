02f167fa636df375ae5ecbbab6fa63720def4be9
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-13 19:46:49 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,5 +2,12 @@ require_relative "./Student.rb"
 
 class Chatty_student < Student
 
+  def hello
+    super
+    "How are you toaday? I'm okay, but I'm kind of tired.  Did you watch the
+    Walking Dead last night? You didn't? Oh man, it was so crazy! What, you
+    don't want any spoileres? Okay well let me just tell you who died..."
+  end
+
 
 end
